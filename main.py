15b8fba5ac7975e02e9321fb25371122806e2e45
dano <dano@c48061fa-0168-4b4d-801e-d588801dec91>
2016-07-09 02:05:29 +0000

    got kivy working well in android

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import kivy
 kivy.require('1.2.0')
+import copy
+import pickle
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -9,9 +11,11 @@ from kivy.properties import ObjectProperty, NumericProperty, ListProperty
 from kivy.factory import Factory
 from kivy.config import ConfigParser
 from kivy.uix.settings import Settings
+from kivy.uix.popup import Popup
+from kivy.uix.floatlayout import FloatLayout
 
 from kivy.core.window import Window
-Window.size = (640, 1163)
+#Window.size = (640, 1163)
 
 import os
 import pkgutil
@@ -103,11 +107,109 @@ if settings.auto_import_functions:
 
 
 ##########
+
+
+class LoadDialog(FloatLayout):
+	load = ObjectProperty(None)
+	cancel = ObjectProperty(None)
+
+class ImportDialog(FloatLayout):
+	import_file = ObjectProperty(None)
+	cancel = ObjectProperty(None)
+
+class SaveDialog(FloatLayout):
+	save = ObjectProperty(None)
+	text_input = ObjectProperty(None)
+	cancel = ObjectProperty(None)
+
+
+
 class Calculator(Widget):
 	entryline = ObjectProperty(None)
 	lastOp = ""
 	operation = False
 	stackdisplay = []
+
+	loadfile = ObjectProperty(None)
+	savefile = ObjectProperty(None)
+	text_input = ObjectProperty(None)
+
+	def dismiss_popup(self):
+		self._popup.dismiss()
+
+	def show_load(self):
+		content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+		self._popup = Popup(title="Load File", content=content,
+							size_hint=(0.9, 0.9))
+		self._popup.open()
+
+	def show_import(self):
+		content = ImportDialog(import_file=self.import_file, cancel=self.dismiss_popup)
+		self._popup = Popup(title="Import File", content=content,
+							size_hint=(0.9, 0.9))
+		self._popup.open()
+
+	def show_save(self):
+		content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+		self._popup = Popup(title="Save file", content=content,
+							size_hint=(0.9, 0.9))
+		self._popup.open()
+
+	def import_file(self, path, filename):
+		filenamefull = os.path.join(path, filename[0])
+		f =  open(filenamefull)
+		commands = f.read()
+		f.close()
+		for command in commands.split('\n'):
+			if len(command) > 0:
+				if command[0] == "#":
+					continue
+				parse(command)
+		interp.message('Imported Instructions from ' + filenamefull)
+		self.dismiss_popup()
+		self.update_screen()
+
+
+	def save(self, path, filename):
+		filenamefull = os.path.join(path, filename)
+		#options['defaultextension'] = '.rpn'
+		#options['filetypes'] = [('all files', '.*'), ('rpn files', '.rpn')]
+		#options['title'] = 'Save Session'
+		#filename = tkFileDialog.asksaveasfilename(filetypes=[('RPN files', '.rpn'), ('all files', '*')], title='Load Session')
+		if filenamefull:
+			interp2 = copy.deepcopy(interp)
+			interp2.builtin_functions = {}
+			interp2.operatorlist = []
+			f = open(filenamefull, 'wb')
+			pickle.dump(interp2, f, 2)
+			f.close()
+			interp.message('Saved as ' + filenamefull)
+			print 'saved: ', filenamefull
+		else:
+			interp.message('Invalid Filename ' + filenamefull)
+
+		self.dismiss_popup()
+		self.update_screen()
+
+	def load(self, path, filename):
+		filenamefull = os.path.join(path, filename[0])
+		global interp
+		if filenamefull:
+			f = open(filenamefull, 'rb')
+			tmp = pickle.load(f)
+			f.close()
+			tmp.builtin_functions = interp.builtin_functions
+			tmp.operatorlist = interp.operatorlist
+			interp = tmp
+
+			interp.message('Loaded ' + filenamefull)
+		else:
+			interp.message('Invalid Filename ' + filenamefull)
+
+		self.dismiss_popup()
+		self.update_screen()
+
+
 
 	def _execute(self, op):
 		if op in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '00', '[', 'space']:
@@ -133,13 +235,16 @@ class Calculator(Widget):
 				interp.step()
 			elif op == 'resume':
 				interp.run()
+		self.update_screen()
 
+	def update_screen(self):
 		stack_size = len(interp.stack)
 		for x in xrange(len(self.stackdisplay)):
 			if x < stack_size:
 				self.stackdisplay[x].value.text = str(interp.stack[-(x + 1)])
 			else:
 				self.stackdisplay[x].value.text = ''
+
 		if interp.messages:
 			self.messageline.value.text = interp.messages[-1]
 		else:
@@ -151,6 +256,7 @@ class Calculator(Widget):
 
 class CalcApp(App):
 	use_kivy_settings = False
+
 	def build(self):
 		self.Calc = Calculator()
 		self.Calc.stackdisplay.append(self.Calc.stackline0)
